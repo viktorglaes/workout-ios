fab9bb877c393a77c4c3f4684b594304a417c9e6
Viktor Glaes <viktorglaes@gmail.com>
2020-09-16 10:00:09 -0400

    Initial Commit

---
+++ Exercise-iOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Exercise-iOS
+//
+//  Created by Viktor Glaes on 9/16/20.
+//  Copyright © 2020 Viktor Glaes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Exercise-iOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Exercise-iOS
+//
+//  Created by Viktor Glaes on 9/16/20.
+//  Copyright © 2020 Viktor Glaes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
